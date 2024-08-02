f468f615e4373536476fadcdae333d549aa2a5bd
SukMaster <110475898+SukMaster@users.noreply.github.com>
2024-08-02 21:52:57 +0900

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'cerulean'
+games { 'gta5' }
+
+name 'Pause-Menu'
+description 'Pause Menu'
+authors 'SukMaster'
+
+client_scripts {
+    'config.lua',
+    'client/main.lua'
+}
+
+server_scripts {
+    'config.lua',
+    'server/main.lua'
+}
